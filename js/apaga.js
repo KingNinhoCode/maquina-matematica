0108c5799eee2b6237af92bc39a87959e9b30931
KingNinhoCode <54991575+KingNinhoCode@users.noreply.github.com>
2020-03-05 20:32:32 -0300

    Função Limpar Tela
    Adicionado botão clear

--- js/apaga.js
+++ js/apaga.js
@@ -2,3 +2,10 @@ var apagar = document.getElementById("apagar");
 apagar.addEventListener("click", function(){
 	tela.textContent = tela.textContent.replace(/.$/, "");
 })
+
+var clr = document.getElementById("clear");
+clr.addEventListener( "click", clear )
+
+function clear(){
+	tela.textContent = "";
+}
